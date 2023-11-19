c5c399aab1708222c0f779ffdada31767e194203
Izyk <lance_hemphill1234@yahoo.com>
2023-11-19 13:46:53 -0500

    destructed props attribute on page

--- src/searchPage.js
+++ src/searchPage.js
@@ -5,31 +5,31 @@ import "aos/dist/aos.css";
 import "./App.css"
 
 
-const SearchPage = (props) => {
+const SearchPage = ({ movie, getDefaultMovies, getMovieData, input, setInput }) => {
 
     useEffect(() => {
-        props.getDefaultMovies();
+        getDefaultMovies();
         AOS.init()
     }, []);
  
     useEffect(() => {  
-        if (props.input.length === 0) {
-            props.getDefaultMovies();
-          } else if (props.input.length >= 1) {
-            props.getMovieData();
+        if (input.length === 0) {
+            getDefaultMovies();
+          } else if (input.length >= 1) {
+            getMovieData();
           }
-    }, [props.input])
+    }, [input])
 
   return (
     <div className="App">
         <div className='searchBarContainer'>
           <div>
             <h1>Search Movies, Shows, and Games</h1>
-            <input onChange={((e) => props.setInput(e.target.value))} id='movieSearch'/>
+            <input onChange={((e) => setInput(e.target.value))} id='movieSearch'/>
           </div>
         </div>
         <div className='resultContainer'>
-            {props.movie.map((Search) => (
+            {movie.map((Search) => (
                 <div key={Search.imdbID} className='movieCards'>
                 <img id='movie' alt='Could not load' src={Search.Poster}/>
                 <p id='title'>{Search.Title}</p>
