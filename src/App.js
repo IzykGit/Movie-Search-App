a51a13f63ed66cfab8c270124af811eb6dce9b0b
Izyk <lance_hemphill1234@yahoo.com>
2023-11-19 13:39:09 -0500

    moved data request to be handled on App page, added props for other pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieDetails from './movieDetails'
 import SearchPage from "./searchPage";
@@ -6,11 +6,36 @@ import SearchPage from "./searchPage";
 
 
 const App = () => {
+
+  const [movie, setMovie] = useState([])
+  const [input, setInput] = useState("")
+
+
+  const getDefaultMovies = async () => {
+    try {
+        const response = await fetch("http://www.omdbapi.com/?s=avengers&apikey=e5c057b"); 
+        const data = await response.json();
+        setMovie(data.Search || []);
+    }   catch (err) {
+    console.log(`Failed to get default movies: ${err}`);
+    }
+  };
+
+    const getMovieData = async () => {
+    try {
+      const response = await fetch (`http://www.omdbapi.com/?s=${input}&apikey=e5c057b`) 
+      const data = await response.json();
+      setMovie(data.Search || [])
+      console.log(data.Search)
+    } catch (err) {
+      console.log(`Failed to get movie ${err}`)
+    }
+    }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/movie_details" element={<MovieDetails />} />
+        <Route path="/" element={<SearchPage input={input} setInput={setInput} getMovieData={getMovieData} getDefaultMovies={getDefaultMovies} movie={movie}/>} />
+        <Route path="/movie_details" element={<MovieDetails movie={movie}/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/searchPage.js
+++ src/searchPage.js
@@ -1,63 +1,35 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css"
 
 
-const SearchPage = () => {
-
-    const [movie, setMovie] = useState([])
-    const [input, setInput] = useState("")
+const SearchPage = (props) => {
 
     useEffect(() => {
-        getDefaultMovies();
+        props.getDefaultMovies();
         AOS.init()
     }, []);
-
+ 
     useEffect(() => {  
-        if (input.length === 0) {
-            getDefaultMovies();
-          } else if (input.length >= 1) {
-            getMovieData();
+        if (props.input.length === 0) {
+            props.getDefaultMovies();
+          } else if (props.input.length >= 1) {
+            props.getMovieData();
           }
-    }, [input])
-
-    const getDefaultMovies = async () => {
-        try {
-            const response = await fetch("http://www.omdbapi.com/?s=avengers&apikey=e5c057b"); 
-            const data = await response.json();
-            setMovie(data.Search || []);
-        }   catch (err) {
-      console.log(`Failed to get default movies: ${err}`);
-        }
-    };
-  
-  const getMovieData = async () => {
-    try {
-      const response = await fetch (`http://www.omdbapi.com/?s=${input}&apikey=e5c057b`) 
-      const data = await response.json();
-      setMovie(data.Search || [])
-      console.log(data.Search)
-    } catch (err) {
-      console.log(`Failed to get movie ${err}`)
-    }
-  }
-  
-
-
-
+    }, [props.input])
 
   return (
     <div className="App">
         <div className='searchBarContainer'>
           <div>
-            <h1>Search Movie</h1>
-            <input onChange={((e) => setInput(e.target.value))} id='movieSearch'/>
+            <h1>Search Movies, Shows, and Games</h1>
+            <input onChange={((e) => props.setInput(e.target.value))} id='movieSearch'/>
           </div>
         </div>
         <div className='resultContainer'>
-            {movie.map((Search) => (
+            {props.movie.map((Search) => (
                 <div key={Search.imdbID} className='movieCards'>
                 <img id='movie' alt='Could not load' src={Search.Poster}/>
                 <p id='title'>{Search.Title}</p>
