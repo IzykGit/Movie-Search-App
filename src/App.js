59b6467b44007a23e4841ce431b35e5d5764746a
Izyk <lance_hemphill1234@yahoo.com>
2023-11-19 01:32:57 -0500

    added react routes, added moviedetails page, create new home page

--- src/App.js
+++ src/App.js
@@ -1,69 +1,19 @@
-import { useState, useEffect } from 'react'
-import './App.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MovieDetails from './movieDetails'
+import SearchPage from "./searchPage";
+
 
 
 const App = () => {
-
-  const [movie, setMovie] = useState([])
-  const [input, setInput] = useState("")
-
-  useEffect(() => {
-    getDefaultMovies();
-    AOS.init()
-  }, []);
-
-  useEffect(() => {  
-    return () => {
-      getMovieData()
-    }
-  }, [input])
-
-  const getDefaultMovies = async () => {
-    try {
-      const response = await fetch("http://www.omdbapi.com/?s=avengers&apikey=e5c057b"); 
-      const data = await response.json();
-      setMovie(data.Search || []);
-    } catch (err) {
-      console.log(`Failed to get default movies: ${err}`);
-    }
-  };
-  
-  const getMovieData = async () => {
-    try {
-      const response = await fetch (`http://www.omdbapi.com/?s=${input}&apikey=e5c057b`) 
-      const data = await response.json();
-      setMovie(data.Search || [])
-      console.log(data.Search)
-    } catch (err) {
-      console.log(`Failed to get movie ${err}`)
-    }
-  }
-  
-
-
-
-
   return (
-    <div className="App">
-        <div className='searchBarContainer'>
-          <div>
-            <h1>Search Movie</h1>
-            <input onChange={((e) => setInput(e.target.value))} id='movieSearch'/>
-          </div>
-        </div>
-        <div className='resultContainer'>
-            {movie.map((Search) => (
-                <div key={Search.imdbID} className='movieCards'>
-                <img id='movie' alt='Could not load' src={Search.Poster}/>
-                <p id='title'>{Search.Title}</p>
-              </div>
-            ))}
-        </div>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/movie_details" element={<MovieDetails />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
 
 export default App;
---
+++ src/searchPage.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./App.css"
+
+
+const SearchPage = () => {
+
+    const [movie, setMovie] = useState([])
+    const [input, setInput] = useState("")
+
+    useEffect(() => {
+        getDefaultMovies();
+        AOS.init()
+    }, []);
+
+    useEffect(() => {  
+        return () => {
+        getMovieData()
+        }
+    }, [input])
+
+    const getDefaultMovies = async () => {
+        try {
+            const response = await fetch("http://www.omdbapi.com/?s=avengers&apikey=e5c057b"); 
+            const data = await response.json();
+            setMovie(data.Search || []);
+        }   catch (err) {
+      console.log(`Failed to get default movies: ${err}`);
+        }
+    };
+  
+  const getMovieData = async () => {
+    try {
+      const response = await fetch (`http://www.omdbapi.com/?s=${input}&apikey=e5c057b`) 
+      const data = await response.json();
+      setMovie(data.Search || [])
+      console.log(data.Search)
+    } catch (err) {
+      console.log(`Failed to get movie ${err}`)
+    }
+  }
+  
+
+
+
+
+  return (
+    <div className="App">
+        <div className='searchBarContainer'>
+          <div>
+            <h1>Search Movie</h1>
+            <input onChange={((e) => setInput(e.target.value))} id='movieSearch'/>
+          </div>
+        </div>
+        <div className='resultContainer'>
+            {movie.map((Search) => (
+                <div key={Search.imdbID} className='movieCards'>
+                <img id='movie' alt='Could not load' src={Search.Poster}/>
+                <p id='title'>{Search.Title}</p>
+              </div>
+            ))}
+        </div>
+    </div>
+  );
+    
+}
+
+export default SearchPage;
